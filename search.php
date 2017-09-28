f062a1060ccc46179887f81d0ea4d9013d39d821
rebstan97 <rebecca_tan_97@hotmail.com>
2017-09-28 12:14:17 +0800

    Added searchpage

---
+++ search.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>CS2102 Project</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+	body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
+	
+	.w3-bar .w3-button {
+    padding: 20px;
+	}
+	
+	.jumbotron {
+	padding-top: 120px;
+	padding-bottom: 40px;
+	background-color: #FEFEFE;
+	}
+	
+	.panel {
+	text-align: center;
+	}
+	
+	.container {
+	text-align: center;
+	}
+
+  </style>
+</head>
+<body>
+
+<div class="w3-top">
+  <div class="w3-bar w3-white w3-card-2" id="myNavbar">
+    <a href="homePage.html" class="w3-bar-item w3-button w3-wide">RENTAL</a>
+    <!-- Right-sided navbar links -->
+    <div class="w3-right w3-hide-small">
+	  <a href="homePage.html" class="w3-bar-item w3-button"><i class="fa fa-home"></i> HOME</a>
+      <a href="aboutUs.html" class="w3-bar-item w3-button"><i class="fa fa-info-circle"></i> ABOUT</a>
+      <a href="dashBoard.html" class="w3-bar-item w3-button"><i class="fa fa-user"></i> DASHBOARD</a>
+      <a href="contactUs.html" class="w3-bar-item w3-button"><i class="fa fa-envelope"></i> CONTACT</a>
+    </div>
+  </div>
+</div>
+
+<div class="jumbotron">
+  <div class="container text-center">
+    <h2>Browse over 1,000 taskers with the skills you need</h2>
+  </div>
+</div>
+
+<div class="container">    
+  <div class="row">
+    <div class="col-sm-4">
+      <div class="panel panel-info">
+        <div class="panel-heading"><b>HOLIDAY PLANNER</b></div>
+        <div class="panel-body"><img src="https://www.eastmeeteast.com/assets/members/chineseguysingle-bf4c3edc7c4fbb7b4c91eedd123e994f4ceac3f536c8065ee07e58af8169b861.jpg" class="img-responsive" style="width:304px;height:228px;" alt="Image"></div>
+        <div class="panel-footer button">
+			<div class="w3-button">Click here for job description</div>
+		</div>
+      </div>
+    </div>
+    <div class="col-sm-4"> 
+      <div class="panel panel-danger">
+        <div class="panel-heading"><b>HOUSING AGENT</b></div>
+        <div class="panel-body"><img src="http://www.personalexcellenceprogramme.com/wp-content/uploads/2013/02/chinese-on-mobile-phone-e1361194133305-300x284.jpg" class="img-responsive" style="width:304px;height:228px" alt="Image"></div>
+        <div class="panel-footer">
+			<div class="w3-button">Click here for job description</div>
+		</div>
+      </div>
+    </div>
+    <div class="col-sm-4"> 
+      <div class="panel panel-success">
+        <div class="panel-heading"><b>PRIMARY SCHOOL TUITION TEACHER</b></div>
+        <div class="panel-body"><img src="https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAUwAAAAJDk5MmJiMzBlLTY5OWUtNGJlOS05MmFmLWQyZGNkZWI4MmNjZQ.jpg" class="img-responsive" style="width:304px;height:228px" alt="Image"></div>
+        <div class="panel-footer">
+			<div class="w3-button">Click here for job description</div>
+		</div>
+      </div>
+    </div>
+  </div>
+</div><br>
+
+<div class="container">    
+  <div class="row">
+    <div class="col-sm-4">
+      <div class="panel panel-danger">
+        <div class="panel-heading"><b>WEB DEVELOPER</b></div>
+        <div class="panel-body"><img src="https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAA2cAAAAJGY5MzI4M2ZmLTBlMjAtNDllZC05MTZjLWIwZThkYTUyYWU0Yg.jpg" class="img-responsive" style="width:304px;height:228px" alt="Image"></div>
+        <div class="panel-footer">Buy 50 mobiles and get a gift card</div>
+      </div>
+    </div>
+    <div class="col-sm-4"> 
+      <div class="panel panel-success">
+        <div class="panel-heading"><b>HOME MAKER</b></div>
+        <div class="panel-body"><img src="https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAA19AAAAJDkxMjlkODY2LTNiZDktNDgxNy1iNzBiLTE0NmE1Y2NjOGFjZA.jpg" class="img-responsive" style="width:304px;height:228px" alt="Image"></div>
+        <div class="panel-footer">
+			<div class="w3-button">Click here for job description</div>
+		</div>
+      </div>
+    </div>
+    <div class="col-sm-4"> 
+      <div class="panel panel-info">
+        <div class="panel-heading"><b>FINANCE MANAGER</b></div>
+        <div class="panel-body"><img src="https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/3/000/278/36e/3d18c27.jpg" class="img-responsive" style="width:304px;height:228px" alt="Image"></div>
+        <div class="panel-footer">
+			<div class="w3-button">Click here for job description</div>
+		</div>
+      </div>
+    </div>
+  </div>
+</div><br><br>
+
+<footer class="w3-center w3-black w3-padding-64">
+  <a href="#home" class="w3-button w3-light-grey"><i class="fa fa-arrow-up w3-margin-right"></i>To the top</a>
+  <div class="w3-xlarge w3-section">
+    <i class="fa fa-facebook-official w3-hover-opacity"></i>
+    <i class="fa fa-instagram w3-hover-opacity"></i>
+    <i class="fa fa-snapchat w3-hover-opacity"></i>
+    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+    <i class="fa fa-twitter w3-hover-opacity"></i>
+    <i class="fa fa-linkedin w3-hover-opacity"></i>
+  </div>
+  <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" title="W3.CSS" target="_blank" class="w3-hover-text-green">w3.css</a></p>
+</footer>
+
+</body>
+</html>
